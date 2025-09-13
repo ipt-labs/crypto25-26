657ede6bda0bf61edfc9fa308d0ceae6aeefe5f9
just_user <avasalatij@gmail.com>
2025-09-13 23:24:51 +0300

    added logo; script was made more versatile - receiving file path from args, automatic file type and encoding detection were added; output was made colored by using colorama

--- lab1/vasalatii_fb-31_yakovchuk_fb-31_cp1/lab1.py
+++ lab1/vasalatii_fb-31_yakovchuk_fb-31_cp1/lab1.py
@@ -1,38 +1,67 @@
+import argparse
 from collections import Counter
+from colorama import Fore, Style
+from chardet.universaldetector import UniversalDetector
+import magic
 import math
+import os
 
-def calc_monogram_entropy(frequency_dict: Counter, n: int) -> float:
-	entropy = 0
-	for char, el in frequency_dict.items():
-		frequency = el/n
-		print(f"Symbol {char} frequency: {frequency}")
-		entropy -=frequency * math.log2(frequency)
-	return entropy
+def print_error(error: str):
+    print(Fore.RED + error + Style.RESET_ALL)
 
-def calc_bigram_entropy(frequency_dict: Counter, n: int) -> float:
+def calc_entropy(frequencies:list[float]) -> float:
 	entropy = 0
-	for char, el in frequency_dict.items():
-		frequency = el / n
-		print(f"Symbol {char} frequency: {frequency}")
+	for frequency in frequencies:
 		entropy -= frequency * math.log2(frequency)
 	return entropy
 
-def parse_file(filename) -> str:
-	with open(filename, encoding="ibm866") as f:
-		txt = f.read()
-		return txt
+def process_text(content:str):
+	#TODO filter text (e.g. remove numbers, special chars, make all lowercase)
+    buffer = content
+    buffer.replace('\n', '')
+    n = len(buffer)
+    print(f"{Fore.LIGHTGREEN_EX}Text length:{Fore.LIGHTBLUE_EX} {n}{Style.RESET_ALL}")
+    monogram_counts = Counter(buffer)
+    bigrams_with_overlapping_counts = Counter(buffer[index:index+2] for index in range(0, n-1))
+	#TODO do not forget about bigrams with overlapping
 
-def main():
-	file_path = "../../tasks/cp1/TEXT"
-	buffer = parse_file(file_path)
-	buffer.replace('\n', '')
-	print(buffer)
-	n = len(buffer)
-	frequency_dict1 = Counter(buffer)
-	mono_entropy = calc_monogram_entropy(frequency_dict1, n)
-	frequency_dict2 = Counter(buffer[index:index+2] for index in range(0, n-1))
-
-	print(f"Entropy of the given text: {round(entropy, 4)}")
 
 if __name__ == "__main__":
-	main()
+	description = Fore.LIGHTBLUE_EX + r"""
+   _____         _      _                _                    
+  |_   _|____  _| |_   / \   _ __   __ _| |_   _ _______ _ __ 
+    | |/ _ \ \/ / __| / _ \ | '_ \ / _` | | | | |_  / _ \ '__|
+    | |  __/>  <| |_ / ___ \| | | | (_| | | |_| |/ /  __/ |   
+    |_|\___/_/\_\\__/_/   \_\_| |_|\__,_|_|\__, /___\___|_|   
+                                            |___/              
+        """ + Style.RESET_ALL
+	parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-f",dest="file",type=str, required=True, help="File to analyze")
+	args = parser.parse_args()
+	print(description)
+	file_path = args.file
+	if not os.path.exists(file_path):
+		print_error(f"{file_path} does not exists")
+	else:
+		print(Fore.LIGHTGREEN_EX + 35*"==" + "\n" + 35*"==" + Style.RESET_ALL)
+		file_type = magic.from_file(args.file,mime=True)
+		print(f"{Fore.LIGHTGREEN_EX}File type of {file_path}:{Fore.LIGHTBLUE_EX} {file_type}{Style.RESET_ALL}")
+		if "text" not in file_type:
+			print_error("Please, provide text file")
+		else:
+			detector = UniversalDetector()
+			with open(file_path, 'rb') as f:
+				for line in f.readlines():
+					detector.feed(line)
+					if detector.done: break
+					detector.close()
+			detection_result = detector.result
+			if detection_result:
+				#TODO it would be better to warn about low confidence
+				print(f"{Fore.LIGHTGREEN_EX}Encoding detection result:{Fore.LIGHTBLUE_EX} {detection_result}{Style.RESET_ALL}")
+				with open(file_path, encoding=detector.result['encoding']) as f:
+					content = f.read()
+					process_text(content)
+			else:
+				print_error("Failed to detect encoding")	
+		
