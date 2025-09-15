4d59d8e866939245b37d4e048ca339703212bba0
saulbadman3 <yakovchuk.o341@gmail.com>
2025-09-15 22:11:19 +0300

    fixed typos, added large txt for analysis, sorted monograms

--- lab1/vasalatii_fb-31_yakovchuk_fb-31_cp1/lab1.py
+++ lab1/vasalatii_fb-31_yakovchuk_fb-31_cp1/lab1.py
@@ -9,24 +9,25 @@ import os
 
 alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
 
-def print_error(error: str):
-    print(Fore.RED + error + Style.RESET_ALL)
+def print_error(error: str) -> None:
+	print(Fore.RED + error + Style.RESET_ALL)
 
 
-def print_df(frame, title=None):
-    table_str = tabulate(frame, headers='keys', tablefmt="heavy_grid", showindex=True)
-    table_width = max(len(line) for line in table_str.splitlines())
+def print_df(frame: pd.DataFrame, title: str=None) -> None:
+	table_str = tabulate(frame, headers='keys', tablefmt="heavy_grid", showindex=True)
+	table_width = max(len(line) for line in table_str.splitlines())
 
-    if title:
-        print(Fore.LIGHTGREEN_EX + title.center(table_width) + Style.RESET_ALL)
+	if title:
+		print(Fore.LIGHTGREEN_EX + title.center(table_width) + Style.RESET_ALL)
 
-    print(Fore.LIGHTCYAN_EX + table_str + Style.RESET_ALL)
+	print(Fore.LIGHTCYAN_EX + table_str + Style.RESET_ALL)
 
 
-def print_green_blue_colored_pair(tag, value, indentation=''):
-    print(indentation + Fore.LIGHTGREEN_EX + tag + " " + Fore.LIGHTBLUE_EX + str(value) + Style.RESET_ALL)
 
-def monogram_occurences(text:str,include_ws:bool) -> tuple[dict[str, int], int]:
+def print_green_blue_colored_pair(tag: str, value: str | int | float | dict, indentation: str='') -> None:
+	print(indentation + Fore.LIGHTGREEN_EX + tag + " " + Fore.LIGHTBLUE_EX + str(value) + Style.RESET_ALL)
+
+def monogram_occurrences(text: str, include_ws: bool) -> tuple[dict[str, int], int]:
 	prev_ch = None
 	total = 0
 	monograms_count = {c: 0 for c in alphabet + (" " if include_ws else "")}
@@ -39,10 +40,10 @@ def monogram_occurences(text:str,include_ws:bool) -> tuple[dict[str, int], int]:
 		monograms_count[temp_ch] += 1
 		total += 1
 		prev_ch = temp_ch
-	return (monograms_count, total)
+	return monograms_count, total
 
 
-def bigram_occurences(text:str,include_ws:bool, overlapped: bool) -> tuple[dict[str, int], int]:
+def bigram_occurrences(text: str, include_ws: bool, overlapped: bool) -> tuple[dict[str, int], int]:
 	temp_alphabet = alphabet + (" " if include_ws else "")
 	bigram_count = {c1+c2:0 for c1 in temp_alphabet for c2 in temp_alphabet}
 	total = 0
@@ -59,7 +60,7 @@ def bigram_occurences(text:str,include_ws:bool, overlapped: bool) -> tuple[dict[
 			total += 1
 		prev_ch = temp_ch
 		index += 1
-	return (bigram_count, total)
+	return bigram_count, total
 
 
 def calculate_ngram_frequencies(ngrams_count: dict[str, int], ngrams_total: int) -> dict[str, float]:
@@ -75,64 +76,64 @@ def calc_entropy(frequencies:dict[str, float]) -> float:
 		frequency = frequencies[bigram]
 		if frequency > 0:
 			entropy -= frequency * math.log2(frequency)
-	return entropy	
+	return entropy
 
 
-def bigram_stat_dict_to_dataframe(bigram_count: dict[str,int]) -> pd.DataFrame:
-    letters = sorted({letter for bigram in bigram_count.keys() for letter in bigram})
-    data = {c1: [bigram_count[c1+c2] for c2 in letters] for c1 in letters}
-    df = pd.DataFrame(data, index=letters)
-    return df.T
+def bigram_stat_dict_to_dataframe(bigram_count: dict[str, float | int]) -> pd.DataFrame:
+	letters = sorted({letter for bigram in bigram_count.keys() for letter in bigram})
+	data = {c1: [bigram_count[c1+c2] for c2 in letters] for c1 in letters}
+	df = pd.DataFrame(data, index=letters)
+	return df.T
 
 
-def process_text(text:str):
+def process_text(text:str, stats_filepath: str):
 	print_green_blue_colored_pair("Text length:", len(text))
 
 	# prepare text
 	filtered_text = text.strip().lower().replace("ё","е").replace("ъ","ь")
 
 	# ngram counts and frequencies will be saved in different sheets of xlsx file
-	writer = pd.ExcelWriter('lab_results.xlsx',engine='xlsxwriter')   
+	writer = pd.ExcelWriter(stats_filepath, engine='xlsxwriter')
 
 	# count monograms
-	(monogram_occurences_without_ws, monogram_total) = monogram_occurences(filtered_text, False)
-	pd.DataFrame(monogram_occurences_without_ws.items(),columns=["Monogram","Count"], index=None).to_excel(writer, sheet_name="CO_MG", index=False)
-	(monogram_occurences_ws, monogram_total_ws) = monogram_occurences(filtered_text, True)
-	pd.DataFrame(monogram_occurences_ws.items(),columns=["Monogram","Count"], index=None).to_excel(writer, sheet_name="CO_MG_WS", index=False)
+	monogram_occurrences_without_ws, monogram_total = monogram_occurrences(filtered_text, False)
+	pd.DataFrame(monogram_occurrences_without_ws.items(),columns=["Monogram","Count"], index=None).sort_values(by="Count", ascending=False).to_excel(writer, sheet_name="CO_MG", index=False)
+	monogram_occurrences_ws, monogram_total_ws = monogram_occurrences(filtered_text, True)
+	pd.DataFrame(monogram_occurrences_ws.items(),columns=["Monogram","Count"], index=None).sort_values(by="Count", ascending=False).to_excel(writer, sheet_name="CO_MG_WS", index=False)
 
 	# count bigrams without overlapping
-	(not_overlapped_bigrams_occurences, not_overlapped_bigrams_total) = bigram_occurences(filtered_text, False, False)
-	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_occurences).to_excel(writer,sheet_name="CO_BG_NOT_OV")
-	(not_overlapped_bigrams_occurences_ws, not_overlapped_bigrams_total_ws) = bigram_occurences(filtered_text, True, False)
-	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_occurences_ws).to_excel(writer,sheet_name="CO_BG_NOT_OV_WS")
+	not_overlapped_bigrams_occurrences, not_overlapped_bigrams_total = bigram_occurrences(filtered_text, False, False)
+	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_occurrences).to_excel(writer,sheet_name="CO_BG_NOT_OV")
+	not_overlapped_bigrams_occurrences_ws, not_overlapped_bigrams_total_ws = bigram_occurrences(filtered_text, True, False)
+	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_occurrences_ws).to_excel(writer,sheet_name="CO_BG_NOT_OV_WS")
 
 	# count bigrams with overlapping
-	(overlapping_bigrams_occurences, overlapping_bigrams_total) = bigram_occurences(filtered_text, False, True)
-	bigram_stat_dict_to_dataframe(overlapping_bigrams_occurences).to_excel(writer,sheet_name="CO_BG_OV")
-	(overlapping_bigrams_occurences_ws, overlapping_bigrams_total_ws) = bigram_occurences(filtered_text, True, True)
-	bigram_stat_dict_to_dataframe(overlapping_bigrams_occurences_ws).to_excel(writer,sheet_name="CO_BG_OV_WS")
+	overlapping_bigrams_occurrences, overlapping_bigrams_total = bigram_occurrences(filtered_text, False, True)
+	bigram_stat_dict_to_dataframe(overlapping_bigrams_occurrences).to_excel(writer,sheet_name="CO_BG_OV")
+	overlapping_bigrams_occurrences_ws, overlapping_bigrams_total_ws = bigram_occurrences(filtered_text, True, True)
+	bigram_stat_dict_to_dataframe(overlapping_bigrams_occurrences_ws).to_excel(writer,sheet_name="CO_BG_OV_WS")
 
 	# calculate monogram frequencies
-	monogram_frequencies = calculate_ngram_frequencies(monogram_occurences_without_ws, monogram_total)
-	pd.DataFrame(monogram_frequencies.items(),columns=["Frequency","Count"]).to_excel(writer, sheet_name="MG_FR", index=False)
-	monogram_frequencies_ws = calculate_ngram_frequencies(monogram_occurences_ws, monogram_total_ws)
-	pd.DataFrame(monogram_frequencies_ws.items(),columns=["Frequency","Count"]).to_excel(writer, sheet_name="MG_FR_WS", index=False)
+	monogram_frequencies = calculate_ngram_frequencies(monogram_occurrences_without_ws, monogram_total)
+	pd.DataFrame(monogram_frequencies.items(),columns=["Monogram","Frequency"]).sort_values(by="Frequency", ascending=False).to_excel(writer, sheet_name="MG_FR", index=False)
+	monogram_frequencies_ws = calculate_ngram_frequencies(monogram_occurrences_ws, monogram_total_ws)
+	pd.DataFrame(monogram_frequencies_ws.items(),columns=["Monogram","Frequency"]).sort_values(by="Frequency", ascending=False).to_excel(writer, sheet_name="MG_FR_WS", index=False)
 
 	# calculate bigrams without overlapping frequencies
-	not_overlapped_bigrams_frequencies = calculate_ngram_frequencies(not_overlapped_bigrams_occurences, not_overlapped_bigrams_total)
+	not_overlapped_bigrams_frequencies = calculate_ngram_frequencies(not_overlapped_bigrams_occurrences, not_overlapped_bigrams_total)
 	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_frequencies).to_excel(writer,sheet_name="BG_FR_NOT_OV")
-	not_overlapped_bigrams_frequencies_ws = calculate_ngram_frequencies(not_overlapped_bigrams_occurences_ws, not_overlapped_bigrams_total_ws)
+	not_overlapped_bigrams_frequencies_ws = calculate_ngram_frequencies(not_overlapped_bigrams_occurrences_ws, not_overlapped_bigrams_total_ws)
 	bigram_stat_dict_to_dataframe(not_overlapped_bigrams_frequencies_ws).to_excel(writer,sheet_name="BG_FR_NOT_OV_WS")
 
 	# calculate bigrams with overlapping frequencies
-	overlapping_bigrams_frequencies = calculate_ngram_frequencies(overlapping_bigrams_occurences, overlapping_bigrams_total)
+	overlapping_bigrams_frequencies = calculate_ngram_frequencies(overlapping_bigrams_occurrences, overlapping_bigrams_total)
 	bigram_stat_dict_to_dataframe(overlapping_bigrams_frequencies).to_excel(writer,sheet_name="BG_FR_OV")
-	overlapping_bigrams_frequencies_ws = calculate_ngram_frequencies(overlapping_bigrams_occurences_ws, overlapping_bigrams_total_ws)
+	overlapping_bigrams_frequencies_ws = calculate_ngram_frequencies(overlapping_bigrams_occurrences_ws, overlapping_bigrams_total_ws)
 	bigram_stat_dict_to_dataframe(overlapping_bigrams_frequencies_ws).to_excel(writer,sheet_name="BG_FR_OV_WS")
 
 	# close writer
 	writer.close()
-	print_green_blue_colored_pair(f"Monograms and bigrams stats were saved to",os.path.join(os.getcwd(),'lab_results.xlsx'))
+	print_green_blue_colored_pair(f"Monograms and bigrams stats were saved to", os.path.join(os.getcwd(), stats_filepath))
 
 	# calculate entropy via monogram frequencies
 	entropy_via_monogram_frequencies = calc_entropy(monogram_frequencies)
@@ -187,7 +188,7 @@ if __name__ == "__main__":
 				print_green_blue_colored_pair("Encoding detection result:", detection_result)
 				with open(file_path, encoding=detector.result['encoding']) as f:
 					content = f.read()
-					process_text(content)
+					process_text(content, f'{os.path.basename(file_path).split('.')[0]}_stats.xlsx')
 			else:
-				print_error("Failed to detect encoding")	
+				print_error("Failed to detect encoding")
 		
