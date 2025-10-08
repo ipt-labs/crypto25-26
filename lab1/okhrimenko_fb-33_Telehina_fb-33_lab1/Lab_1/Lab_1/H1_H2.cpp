bb33ab7ed549814140702177cb7a0f94fe16bf9e
ohrimenkoanastasiia <ohrimenko.anastasiia@gmail.com>
2025-10-08 21:12:14 +0300

    H2 + output with frequency of bigrams

--- lab1/okhrimenko_fb-33_Telehina_fb-33_lab1/Lab_1/Lab_1/H1_H2.cpp
+++ lab1/okhrimenko_fb-33_Telehina_fb-33_lab1/Lab_1/Lab_1/H1_H2.cpp
@@ -16,10 +16,21 @@ double entropy1(const map<char, int> freq, int total) {
     return H;
 }
 
+// function for H2
+double entropy2(const map<string, int> freq, int total) {
+    double H = 0;
+    for (auto p : freq) {
+        double prob = (double)p.second / total;
+        if (prob > 0)
+            H -= prob * log2(prob);
+    }
+    return H / 2.0;
+}
+
 int main() {
     ifstream fin("input.txt"); 
     string text, line;
-    while (getline(fin, line)) { //new text without space,numbers etc
+    while (getline(fin, line)) {
         for (char c : line) {
             if (isalpha(c)) {
                 c = tolower(c); 
@@ -34,6 +45,8 @@ int main() {
     map<char, int> freq1;
     for (char c : text) freq1[c]++;
 
+    cout << "For H1: " << endl;
+
     //output is H1+frequency every letter
     for (auto p : freq1) {
         double prob = (double)p.second / total;
@@ -41,6 +54,25 @@ int main() {
     }
 
     cout << "H1 = " << entropy1(freq1, text.size()) << endl;
+    
+    cout << "For H2: " << endl;
+
+    // frequency of bigrams
+    map<string, int> freq2;
+    for (size_t i = 0; i + 1 < text.size(); i++) {
+        string bigram = "";
+        bigram += text[i];
+        bigram += text[i + 1];
+        freq2[bigram]++;
+    }
+
+    //output is H2+frequency every bigram
+    for (auto p : freq2) {
+        double prob = (double)p.second / (total - 1);
+        cout << p.first << "  " << p.second << "  " << prob << endl;
+    }
+
+    cout << "H2 = " << entropy2(freq2, total - 1);
 
     return 0;
 }
