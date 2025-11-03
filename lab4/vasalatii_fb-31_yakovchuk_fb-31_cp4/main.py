8923abadce4af9103584afbfb24ff49c736ce676
saulbadman3 <yakovchuk.o341@gmail.com>
2025-11-03 22:57:17 +0200

    Generating strong primes added

---
+++ lab4/vasalatii_fb-31_yakovchuk_fb-31_cp4/main.py
@@ -0,0 +1,109 @@
+import argparse
+from colorama import Fore, Style
+from styles import print_error, print_green_blue_colored_pair
+import random
+
+def gcd(a: int,b: int) -> int:
+    while b:
+        a, b = b, a % b
+    return a
+
+def mod_pow_horner(a: int, m: int, n: int) -> int:
+    b = 1
+    bin_m = bin(m)[2:]
+
+    for bit in bin_m:
+        b = (b * b) % n
+        if bit == '1':
+            b = (b * a) % n
+    return b
+
+def trial_division(n: int) -> bool:
+    if n < 2:
+        return False
+
+    small_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+    for p in small_primes:
+        if n == p:
+            return True
+        if n % p == 0:
+            return False
+
+    return True
+
+def miller_rabin_test(p: int, k: int=7) -> bool:
+    if not trial_division(p):
+        return False
+
+    d = p - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    count = 0
+
+    while count < k:
+        x = random.randrange(2, p)
+
+        if gcd(x, p) != 1:
+            return False
+        a = mod_pow_horner(x, d, p)
+        if a == 1 or a == p - 1:
+            count += 1
+            continue
+
+        for r in range(1, s):
+            a = mod_pow_horner(a, 2, p)
+            if a == p - 1:
+                break
+        else:
+            return False
+
+        count += 1
+    return True
+
+def generate_strong_prime(*, bits: int=None, start: int=None, end: int=None) -> int:
+    if bits is not None:
+        if start is not None or end is not None:
+            print(f"Start and end would be ignored as certain number of bits was mentioned")
+        start = 2 ** (bits - 1)
+        end = 2 ** (bits + 50)
+    elif start is None or end is None:
+        raise ValueError("Start and end must be provided if number of bits was not mentioned")
+
+    while True:
+        candidate = random.randrange(start, end)
+        if miller_rabin_test(candidate):
+            i = 0
+            while True:
+                i += 1
+                p = 2 * i * candidate + 1
+                if p.bit_length() > end.bit_length():
+                    break
+                if miller_rabin_test(p):
+                    return p
+
+
+def keygen(): #TODO implement key generation function
+    return
+
+if __name__ == "__main__":
+    p1 = generate_strong_prime(bits=256)
+    q1 = generate_strong_prime(bits=256)
+    print_green_blue_colored_pair("P1:", p1)
+    print_green_blue_colored_pair("Q1:", q1)
+    p, q = None, None
+    while True:
+        p = generate_strong_prime(bits=256)
+        max_q = (p1 * q1) // p
+        min_q = 2 ** 255
+        if max_q < min_q:
+            continue
+        q = generate_strong_prime(start=min_q, end=max_q)
+        break
+    print_green_blue_colored_pair("P:", p)
+    print_green_blue_colored_pair("Q:", q)
+    print(p*q <= p1*q1)
+    #TODO implement further program logic
